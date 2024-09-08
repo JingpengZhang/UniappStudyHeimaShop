036797762a699621d076f5504da1072d27761bbc
JingpengZhang <jingpeng_zhang@foxmail.com>
2024-09-08 14:29:19 +0800

    P44 - 商品详情-轮播图交互
    1. 轮播图组件相关事件（swiper）；
    2. 大图预览（uni.previewImage)；

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -22,6 +22,21 @@ const getGoodsData = async () => {
 onLoad(() => {
   getGoodsData()
 })
+
+// 轮播图变化时
+const currentIndex = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  currentIndex.value = ev.detail!.current
+}
+
+// 点击图片时
+const onTapImage = (url: string) => {
+  // 大图预览
+  uni.previewImage({
+    current: url,
+    urls: goods.value!.mainPictures,
+  })
+}
 </script>
 
 <template>
@@ -30,13 +45,13 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper circular @change="onChange" :current="currentIndex">
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+            <image @tap="onTapImage" mode="aspectFill" :src="item" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currentIndex + 1 }}</text>
           <text class="split">/</text>
           <text class="total">5</text>
         </view>
