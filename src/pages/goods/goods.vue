5c9091dd53374a62966f95b8b87ae35aa8929dcf
JingpengZhang <jingpeng_zhang@foxmail.com>
2024-09-08 14:38:57 +0800

    P45 - 商品详情-弹出层
    1. uni-popup 组件使用说明

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -14,7 +14,8 @@ const query = defineProps<{
 // 获取商品详情数据
 const goods = ref<GoodsResult>()
 const getGoodsData = async () => {
-  const res = await getGoodsAPI(query.id)
+  // const res = await getGoodsAPI(query.id)
+  const res = await getGoodsAPI('1225018')
   goods.value = res.result
 }
 
@@ -37,6 +38,12 @@ const onTapImage = (url: string) => {
     urls: goods.value!.mainPictures,
   })
 }
+
+// 弹出层组件ref
+const popup = ref<{
+  open: (type?: 'top' | 'bottom') => void
+  close: () => void
+}>()
 </script>
 
 <template>
@@ -77,7 +84,7 @@ const onTapImage = (url: string) => {
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="popup?.open('bottom')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -147,6 +154,13 @@ const onTapImage = (url: string) => {
       <view class="buynow"> 立即购买 </view>
     </view>
   </view>
+
+  <!-- uni-ui 弹出层组件 -->
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <view>弹出层内容1</view>
+    <view>弹出层内容2</view>
+    <button @tap="popup?.close()">关闭弹出层</button>
+  </uni-popup>
 </template>
 
 <style lang="scss">
